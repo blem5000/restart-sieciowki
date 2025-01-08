ec0f996083247ef57bb903bc2a1570ec72d0a144
blem5000 <blem5000@gmail.com>
2025-01-08 16:04:58 +0100

    Add files via upload

---
+++ restart sieciowki.ps1
@@ -0,0 +1,12 @@
+﻿#Get-PnpDevice | where {$_.Status -notLike "Error" -and $_.FriendlyName -like "*AX211*"}
+
+Start-Sleep 3
+$wifi = Get-PnpDevice | where {$_.FriendlyName -like "*AX211*"}
+if($wifi.Status -notlike "OK"){
+    Disable-PnpDevice $wifi.InstanceId -Confirm:$false
+    Start-Sleep 3
+    Enable-PnpDevice $wifi.InstanceId -Confirm:$false
+    }
+else{
+    Write-Host "Nie ma co restartować, status" $wifi.FriendlyName "jest" $wifi.Status
+    }
